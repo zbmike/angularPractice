3096a352290407fcdf8bc4eb8ea26160bb776201
Zhiyuan He <zh2312@columbia.edu>
2020-07-31 00:56:30 -0400

    Add post method to frontend and backend

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -29,7 +29,12 @@ export class PostsService {
 
   addPost(title: string, content: string) {
     const post: Post = { id: null, title, content };
-    this.posts.push(post);
-    this.postsUpdated.next([...this.posts]);
+    this.http
+      .post<{ message: string }>('http://localhost:3000/api/posts', post)
+      .subscribe((response) => {
+        console.log(response);
+        this.posts.push(post);
+        this.postsUpdated.next([...this.posts]);
+      });
   }
 }
