dd8bb85f8219f892ad3c1bef5ef4d5dd4136c8f8
Zhiyuan He <zh2312@columbia.edu>
2020-07-31 00:15:45 -0400

    Add angular http client in front end

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { MatInputModule } from '@angular/material/input';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
@@ -29,6 +30,7 @@ import { PostListComponent } from './posts/post-list/post-list.component';
     MatButtonModule,
     MatToolbarModule,
     MatExpansionModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -1,6 +1,7 @@
 import { Post } from './post.model';
 import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 // dependency injection or add service to providers array
 @Injectable({ providedIn: 'root' })
@@ -8,8 +9,18 @@ export class PostsService {
   private posts: Post[] = [];
   private postsUpdated = new Subject<Post[]>();
 
+  constructor(private http: HttpClient) {}
+
   getPosts() {
-    return [...this.posts];
+    this.http
+      .get<{ meesage: string; posts: Post[] }>(
+        'http://localhost:3000/api/posts'
+      )
+      .subscribe((data) => {
+        this.posts = data.posts;
+        // inform the update
+        this.postsUpdated.next([...this.posts]);
+      });
   }
 
   getPostUpdateListener() {
@@ -17,7 +28,7 @@ export class PostsService {
   }
 
   addPost(title: string, content: string) {
-    const post: Post = { title, content };
+    const post: Post = { id: null, title, content };
     this.posts.push(post);
     this.postsUpdated.next([...this.posts]);
   }
