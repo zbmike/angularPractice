ae38cca57f004235024058f5a647d00f5d1f5934
Zhiyuan He <zh2312@columbia.edu>
2020-08-04 02:13:06 -0400

    Configure multer for file upload in back end

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -1,10 +1,33 @@
 const express = require("express");
+const multer = require("multer");
 
 const Post = require("../models/post");
 
 const router = express.Router();
 
-router.post("/", async (req, res, next) => {
+const MIME_TYPE_MAP = {
+  "image/png": "png",
+  "image/jpg": "jpg",
+  "image/jpeg": "jpg",
+};
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    const isValid = MIME_TYPE_MAP[file.mimetype];
+    let error = new Error("Invalid mime type");
+    if (isValid) {
+      error = null;
+    }
+    cb(error, "backend/image");
+  },
+  filename: (req, file, cb) => {
+    const name = file.originalname.toLowerCase().split(" ").join("-");
+    const ext = MIME_TYPE_MAP[file.mimetype];
+    cb(null, name + "-" + Date.now() + "." + ext);
+  },
+});
+
+router.post("/", mult(storage).single("image"), async (req, res, next) => {
   const post = new Post({
     title: req.body.title,
     content: req.body.content,
